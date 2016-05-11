9d55f73108a1600eefb3180268d01a23aa6db1ef
tutujiaw <tujiaw@163.com>
2016-05-11 22:21:26 +0800

    1111

--- public/javascripts/article.js
+++ public/javascripts/article.js
@@ -4,17 +4,13 @@ $(function() {
     window.open(url, '_blank');
   });
 
-  $(window).scroll(function() {
-    var scrollTop = $(document).scrollTop();
-    var docHeight = $(document).height();
-    var winHeight = $(window).height();
-
-    $('.typeList').css('marginTop', scrollTop);
-    var pageButton = $('.pageButton');
-    if (scrollTop == 0) {
-      $('.articleList').prepend(pageButton);
-    } else if (scrollTop + winHeight == docHeight) {
-      $('.articleList').append(pageButton);
+  $('.articleList').scroll(function() {
+    log($(this).height() + $(this).scrollTop());
+    log($(this)[0].scrollHeight);
+    if ($(this).scrollTop() == 0) {
+      $(this).prepend(pageButton);
+    } else if ($(this).height() + $(this).scrollTop() == $(this)[0].scrollHeight) {
+      $(this).append(pageButton);
     }
   })
 });
